d1c9ad0b62c8cb2ce9ec6250eeb0b59635086ed7
Tiago Bastos <tiagobastostads@gmail.com>
2025-02-08 18:49:10 -0300

    Nova publicação 01

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,9 @@
-//const { defineConfig } = require("cypress");
+const { defineConfig } = require("cypress");
 
-//module.exports = defineConfig({
-//  e2e: {
-//    setupNodeEvents(on, config) {
-      //implement node event listeners here
-//    },
-//  },
-//});
-module.exports = {
+module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
-    viewportWidth: 1280,
-    viewportHeight: 800,
+    setupNodeEvents(on, config) {
+      //implement node event listeners here
+    },
   },
-};
-
+});
