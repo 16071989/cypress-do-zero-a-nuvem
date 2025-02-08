e7cba0b9a3afaa266de244af3886d76522e9fb46
Tiago Bastos <tiagobastostads@gmail.com>
2025-02-07 23:54:23 -0300

    Teste

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
-const { defineConfig } = require("cypress");
+//const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
+//module.exports = defineConfig({
+//  e2e: {
+//    setupNodeEvents(on, config) {
       //implement node event listeners here
-    },
+//    },
+//  },
+//});
+module.exports = {
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    viewportWidth: 1280,
+    viewportHeight: 800,
   },
-});
+};
+
