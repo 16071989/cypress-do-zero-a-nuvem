f949d7606555032d6188fc6b4cb6b7c61a36902d
Tiago Bastos <tiagobastostads@gmail.com>
2025-02-09 16:37:53 -0300

    teste automatizado

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
-    supportFile: 'cypress/support/e2e.js',
-    specPattern: 'cypress/integration/**/*.spec.js',
     setupNodeEvents(on, config) {
       //implement node event listeners here
     },
